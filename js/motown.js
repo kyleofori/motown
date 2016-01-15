43513b5927e8de1e208f8a84ba4866121a7f77ea
Kyle Ofori <kyle.ofori@detroitlabs.com>
2016-01-15 14:24:54 -0500

    Add method to update picture

--- js/motown.js
+++ js/motown.js
@@ -1,12 +1,20 @@
 function updateTitle() {
   var title = document.getElementsByTagName('h1');
-  console.log(title);
   title[0].innerHTML = "The Temptations";
 }
 
+function updatePicture() {
+  var picture = document.getElementById('cookePic');
+  picture.src = "images/temptations.jpg";
+}
+
+function update() {
+  updateTitle();
+  updatePicture();
+}
 var runningText = document.getElementById('running');
 
 // Notice: can't have parentheses after the function names.
-runningText.onclick = updateTitle;
+runningText.onclick = update;
 // Equivalent to
 // runningText.addEventListener('click', updateTitle, false);
