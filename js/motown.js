a70460cbde71d0844e856c7c15c694a917cf2cfc
Kyle Ofori <kyle.ofori@detroitlabs.com>
2016-02-26 12:16:53 -0500

    Change theme of text adventure game and use buttons instead of spans for navigation

--- js/motown.js
+++ js/motown.js
@@ -1,20 +1,62 @@
-function updateTitle() {
+var river = "OK now you're at the river and whatever";
+var compuware = "You are at the Compuware building. (How did you know that?) (It's not called that anymore.)";
+var riverTitle = "Detroit River"
+var compuwareTitle = "One Campus Martius"
+
+function update(e) {
+  updateTitle(e);
+  updatePicture(e);
+  updateText(e);
+  updateChoices(e);
+}
+
+function updateTitle(e) {
   var title = document.getElementsByTagName('h1');
-  title[0].innerHTML = "The Temptations";
+  if(e.target.className == 'choice two') {
+    title[0].innerHTML = riverTitle;
+  } else {
+    title[0].innerHTML = compuwareTitle;
+  }
 }
 
-function updatePicture() {
-  var picture = document.getElementById('cookePic');
-  picture.src = "images/temptations.jpg";
+function updatePicture(e) {
+  var picture = document.getElementById('headerImage');
+  if(e.target.className == 'choice two') {
+    picture.src = "images/hartriverview.jpg";
+  } else {
+    picture.src = "images/compuware.jpg";
+    }
 }
 
-function update() {
-  updateTitle();
-  updatePicture();
+function updateText(e) {
+  var description = document.getElementById('description');
+  console.log(description);
+  console.log("This is the event " + e.target.className);
+  var newText = "default text";
+  if(e.target.className == 'choice two') {
+    newText = river;
+  } else {
+    newText = compuware;
+  }
+  console.log(newText);
+  description.innerHTML = '<p>' + newText + '</p>';
 }
-var runningText = document.getElementById('running');
 
+function updateChoices(e) {
+  var choice = ["Whatcha gonna do?", "One", "Two", "Three", "Four"];
+  var question = document.getElementById('question');
+  if(e.target.className == 'choice two') {
+    question = choice[0];
+  } else {
+    newText = compuware;
+  }
+}
+
+
+var choices = document.getElementsByClassName('choice');
 // Notice: can't have parentheses after the function names.
-runningText.onclick = update;
+console.log(choices);
+choices[0].onclick = update;
+choices[1].onclick = update;
 // Equivalent to
-// runningText.addEventListener('click', updateTitle, false);
+// runningText.addEventListener('click', update, false);
